beeb13eb843d3a74023b6e4c1155cd12629d1e67
Sabelli <sabellinho@gmail.com>
2025-11-15 11:47:42 -0300

    chore: estrutura básica de route Contratacao

---
+++ src/routes/Contratacao/index.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+export default function Contratacao() {
+  useEffect(() => {
+    document.title = "Contratação";
+  }, []);
+  
+  return (
+    <main className="">
+
+    </main>
+  )
+}
