87d28edcadb58180c2fb2b9f05470f0a302966c0
Sabelli <sabellinho@gmail.com>
2025-11-15 11:48:46 -0300

    chore: estrutura básica de route Integrantes

---
+++ src/routes/Integrantes/index.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+export default function Integrantes() {
+  useEffect(() => {
+    document.title = "Integrantes";
+  }, []);
+  
+  return (
+    <main className="">
+
+    </main>
+  )
+}
