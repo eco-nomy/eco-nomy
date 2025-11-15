84a507ceb426b2667ee584d17ea07059a9616168
Sabelli <sabellinho@gmail.com>
2025-11-15 11:49:08 -0300

    chore: estrutura básica de route Integrantes

---
+++ src/routes/Perguntas/index.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+export default function Perguntas() {
+  useEffect(() => {
+    document.title = "Perguntas";
+  }, []);
+  
+  return (
+    <main className="">
+
+    </main>
+  )
+}
