5f1c36a8ea2d5fa9c5095de318d74d11736218da
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-17 20:45:11 -0300

    Chore: Componente rodape adicionado

---
+++ src/components/Rodape/Rodape.tsx
@@ -0,0 +1,50 @@
+import AcessoRapido from '../AcessoRapido/AcessoRapido'
+
+export default function Rodape() {
+  return (
+    <footer style={{ backgroundColor: '#194737' }} className="text-white py-10 px-6">
+      <div className="max-w-7xl mx-auto">
+        <div className="">
+
+          <div>
+            <h1 className="">Acesso Rápido</h1>
+            <AcessoRapido />
+          </div>
+
+          <div>
+            <h1 className="">Redes Sociais</h1>
+            <div className="">
+
+            </div>
+          </div>
+
+          <div>
+            <h1 className="">Atendimento</h1>
+            <p className="">Fale conosco por e-mail ou telefone!</p>
+            <ul className="">
+              <li>
+                <p className="">
+                  economy@email.com
+                </p>
+              </li>
+              <li>
+                <p className="">
+                  11 4444-4444
+                </p>
+              </li>
+              <li>
+                <p className="">
+                  11 9 9999-9999
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <p className="">
+          &copy; Todos os direitos reservados - Eco-nomy - 2025
+        </p>
+      </div>
+    </footer>
+  )
+}
