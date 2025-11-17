a6c625e95fc66b4cd69a0c57aa2ebf8e78b356e2
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-17 20:45:33 -0300

    Chore: Componente AcessoRapido adicionado

---
+++ src/components/AcessoRapido/AcessoRapido.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function AcessoRapido() {
+    return (
+        <nav className="grid grid-cols-2 gap-4 text-lg text-gray-200 place-items-center acesso-items">
+            <Link to="/">Home</Link>
+            <Link to="/integrantes">Integrantes</Link>
+            <Link to="/sobre">Sobre</Link>
+            <Link to="/oficinas">Oficinas</Link>
+            <Link to="/perguntas-frequentes">Perguntas(FAQ)</Link>
+            <Link to="/trabalhos">Trabalhos</Link>
+            <Link to="/contato">Contato</Link>
+        </nav>
+    );
+}
