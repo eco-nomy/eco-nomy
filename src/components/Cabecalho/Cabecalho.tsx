d0cedc346395d47ae7c74710a540a14d8eca74df
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-17 22:30:58 -0300

    Feat: Cabeçalho

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -3,27 +3,32 @@ import Menu from '../Menu/Menu'
 
 export default function Cabecalho() {
     return (
-        <header>
-            <div className='flex flex-col sm:flex-row sm:justify-between sm:items-center p-2'>
-                <div className='flex justify-center'>
-                    <p className='flex items-center gap-1'>
+        <header className="bg-white  text-[#0b521f]  shadow-md">
+            <div className="bg-[#2e7c10] flex flex-col sm:flex-row sm:justify-between sm:items-center text-white px-4 py-2 text-sm ">
+                <div className="flex justify-center">
+                    <p className="flex items-center gap-1">
                         economy@email.com
                     </p>
                 </div>
 
-                <div className='flex justify-center gap-2'>
-                    <p className='flex items-center gap-1'>
+                <div className="flex justify-center gap-4 mt-2 sm:mt-0">
+                    <p className="flex items-center gap-1">
                         11 4444-4444
                     </p>
-                    <p className='flex items-center gap-1'>
+                    <p className="flex items-center gap-1">
                         11 9 9999-9999
                     </p>
                 </div>
             </div>
 
-            <div className='flex'>
+            <div className="flex items-center justify-between px-6 py-4">
                 <div className="flex items-center space-x-2">
-                    <Link to="/">logo</Link>           
+                    <Link 
+                        to="/" 
+                        className="text-2xl font-bold hover:text-[#0b521f]  transition"
+                    >
+                        Eco-nomy
+                    </Link>           
                 </div>
                 <Menu />
             </div>
