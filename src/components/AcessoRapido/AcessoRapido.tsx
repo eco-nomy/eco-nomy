d280b97c43835411f236f4e14a5577450c20054b
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-17 22:30:48 -0300

    Feat: Estilizando acessorapido

--- src/components/AcessoRapido/AcessoRapido.tsx
+++ src/components/AcessoRapido/AcessoRapido.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function AcessoRapido() {
     return (
-        <nav className="grid grid-cols-2 gap-4 text-lg text-gray-200 place-items-center acesso-items">
+        <nav className="grid grid-cols-2 gap-2 text-lg text-gray-200 place-items-center acesso-items">
             <Link to="/">Home</Link>
             <Link to="/integrantes">Integrantes</Link>
             <Link to="/sobre">Sobre</Link>
