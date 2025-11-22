50e3fbeaf7d8c31f8ed5de4214aa5b6749dfa509
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-22 01:16:10 -0300

    feature(Rodape.tsx): Estrutura de layout criada

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -1,3 +1,39 @@
 export default function Rodape() {
- return(); 
+ return(
+    <footer>
+      <div>
+        <div>
+
+          <div>
+            <h1>
+              
+            <h1/>
+          <div/>
+
+          <div>
+            <h1>
+            <h1/>
+            <div>
+            <div/>
+          <div/>
+
+          <div>
+          
+            <h1>
+              
+            <h1/>
+            <p>
+
+            <p/>
+            
+          <div/>
+
+        <div/>
+        
+        <p>
+            
+        <p/>
+      <div/>
+    <footer/>
+ ); 
 }
