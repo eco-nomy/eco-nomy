e26f577291902092a146d21ba3fffc88e2f04670
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-22 00:19:28 -0300

    feature(Menu.tsx): função para alterar o valor de abertura do menu dropdown

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,8 +1,17 @@
+import {useLocation} from "react-router-dom";
+
 export default function Menu() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location])
+  
   return(
       <div>
         <button onClick={
-          ()=> 
+          ()=> setIsOpen(!isOpen) 
         }>
           Menu
         <button/>
