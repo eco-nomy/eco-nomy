040868c9175ff7334d51324e256ca19e23f56bb6
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-21 22:51:06 -0300

    feat(Cabecalho.tsx): adicionado estrutura inicial do corpo do cabecalho

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,5 +1,14 @@
 export default function Cabecalho() {
   return(
+    <header>
+      <div>
+        <div>
+          <p>
+            <img src={}/>
+          </p>
+        </div>
+      </div>
+    </header>
 
   )
 }
