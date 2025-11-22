983ad377a546c175f6a63a0a3f143e1b27d210b9
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-21 21:44:49 -0300

    feat(AcessoRapido.tsx): adicionados endereços de página aos links

--- src/components/AcessoRapido.tsx
+++ src/components/AcessoRapido.tsx
@@ -1,13 +1,13 @@
 export default function AcessoRapido() {
   return(
     <nav>
-      <Link to=""></Link>
-      <Link to=""></Link>
-      <Link to=""></Link>
-      <Link to=""></Link>
-      <Link to=""></Link>
-      <Link to=""></Link>
-      <Link to=""></Link>
+      <Link to="/"></Link>
+      <Link to="/integrantes"></Link>
+      <Link to="/sobre"></Link>
+      <Link to="/oficinas"></Link>
+      <Link to="/perguntas-frequentes"></Link>
+      <Link to="/trabalhos"></Link>
+      <Link to="/contato"></Link>
     </nav>
   );
 }
