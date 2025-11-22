d799ed04841a990601c44ba4a865186651a761fa
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-21 21:40:34 -0300

    feat(AcessoRapido.tsx): Criado layout para Links de navegação

--- src/components/AcessoRapido.tsx
+++ src/components/AcessoRapido.tsx
@@ -1,7 +1,13 @@
 export default function AcessoRapido() {
   return(
     <nav>
-    
+      <Link to=""></Link>
+      <Link to=""></Link>
+      <Link to=""></Link>
+      <Link to=""></Link>
+      <Link to=""></Link>
+      <Link to=""></Link>
+      <Link to=""></Link>
     </nav>
   );
 }
