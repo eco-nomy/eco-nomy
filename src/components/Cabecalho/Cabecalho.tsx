352ad25f7cde1278b5e2ac2715b52d72b69097e1
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-21 23:06:07 -0300

    feat(Cabecalho.tsx): adicionada classes para estilização dos componentes da primeira div do cabeçalho

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,15 +1,14 @@
 export default function Cabecalho() {
   return(
     <header>
-      <div>
-        <div>
-          <p>
-            <img src={}/>
+      <div className='flex flex-col sm:flex-row sm:justify-between sm:items-center p-2'>
+        <div className='flex justify-center'>
+          <p className='flex items-center gap-1'>
+            <img src={} className='w-4 h-4' alt="ícone email" />
             economy@gmail.com
           </p>
         </div>
       </div>
     </header>
-
   )
 }
