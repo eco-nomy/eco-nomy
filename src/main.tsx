82d345e4527ca96528922f2c931812bc33e8f816
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-22 17:59:14 -0300

    Feat: Rota login adicionada

--- src/main.tsx
+++ src/main.tsx
@@ -11,21 +11,32 @@ import Trabalhos from './routes/Trabalhos/index.tsx'
 import Contratacao from './routes/Contratacao/index.tsx'
 import Hub from './routes/Hub/index.tsx'
 import Contato from './routes/Contato/index.tsx'
+import Login from './routes/Login/index.tsx'
+import { ThemeProvider } from './context/ThemeContext'
 
 const router = createBrowserRouter([
-  {path:"/", element:<App/>, errorElement:<Error/>, children:[
-    {path:"/", element:<Home/>},
-    {path:"/sobre", element:<Sobre/>},
-    {path:"/perguntas", element:<Perguntas/>},
-    {path:"/integrantes", element:<Integrantes/>},
-    {path:"/trabalhos", element:<Trabalhos/>},
-    {path:"/contratacao", element:<Contratacao/>},
-    {path:"/hub", element:<Hub/>},
-    {path:"/contato", element:<Contato/>}
-  ]}
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/sobre", element: <Sobre /> },
+      { path: "/perguntas", element: <Perguntas /> },
+      { path: "/integrantes", element: <Integrantes /> },
+      { path: "/trabalhos", element: <Trabalhos /> },
+      { path: "/contratacao", element: <Contratacao /> },
+      { path: "/hub", element: <Hub /> },
+      { path: "/contato", element: <Contato /> },
+      { path: "/login", element: <Login /> }
+    ]
+  }
 ])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
