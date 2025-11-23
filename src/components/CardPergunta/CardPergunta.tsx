2272c312b26d8018cbad10f9f51efc0b4f9517e0
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-23 03:42:36 -0300

    Feat: CardPergunta alterado para mostrar dados ao inves de redirecionar

--- src/components/CardPergunta/CardPergunta.tsx
+++ src/components/CardPergunta/CardPergunta.tsx
@@ -1,19 +1,26 @@
-import { useNavigate } from "react-router-dom";
-import type { CardFaq } from "../../types/cardFaq";
+import { useState } from "react";
+import type { TipoFaq } from "../../types/tipoFaq";
 
-export default function CardPergunta({ faq }: CardFaq) {
-  const navigate = useNavigate();
-
-  const irParaResposta = (id: number) => {
-    navigate(`/perguntas-frequentes/${id}`);
-  };
+export default function CardPergunta({ faq }: { faq: TipoFaq }) {
+  const [aberto, setAberto] = useState(false);
 
   return (
-    <button
-      onClick={() => irParaResposta(faq.id)}
-      className="w-full max-w-[95vw] sm:max-w-md mx-auto text-center bg-[#76b99d] text-[#194737] font-semibold px-4 py-3 rounded hover:bg-white transition shadow-sm"
+    <div
+      onClick={() => setAberto(!aberto)}
+      className="bg-[var(--c-bg2)] text-white cursor-pointer w-full max-w-[95vw] sm:max-w-md mx-auto text-left 
+                 font-semibold px-4 py-3 rounded 
+                 hover:bg-[var(--c-bg1)] transition shadow-sm"
     >
-      {faq.titulo}
-    </button>
+      <p>{faq.titulo}</p>
+
+      {aberto && (
+        <div className="mt-2 text-sm text-white font-normal space-y-1">
+          <p><strong>Pergunta:</strong> {faq.corpo}</p>
+          <p><strong>Autor da pergunta:</strong> {faq.autorDaPergunta}</p>
+          <p><strong>Autor da resposta:</strong> {faq.autorDaResposta}</p>
+          <p><strong>Data da resposta:</strong> {new Date(faq.data_resposta).toLocaleDateString()}</p>
+        </div>
+      )}
+    </div>
   );
 }
