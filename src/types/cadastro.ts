bb21e979650d80b1daf712d31db7d6463e99d181
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-23 19:24:06 -0300

    Fix: Sistema de cadastro corrigido

--- src/types/cadastro.ts
+++ src/types/cadastro.ts
@@ -1,16 +1,18 @@
 export type TipoCadastro = "empresa" | "empregado";
 
 export type CadastroEmpresa = {
-  tipo: "empresa";
-  nome: string;
-  cnpj: string;
-  senha: string;
+    tipo: "empresa";
+    nome: string;
+    cnpj: string;
+    senha: string;
 };
 
 export type CadastroEmpregado = {
-  tipo: "empregado";
-  nome: string;
-  cpf: string;
-  email: string;
-  senha: string;
+    tipo: "empregado";
+    nome: string;
+    cpf: string;
+    email: string;
+    saldo: number;
+    dataCriacao: string;
+    senha: string;
 };
