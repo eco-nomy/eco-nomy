4203ec7bfaf963ec670bcc3dbe67f794b7ba9d7d
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-23 03:49:36 -0300

    Feat: Página Error estruturada

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,13 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Error() {
   useEffect(() => {
-    document.title = "Error";
+    document.title = "Erro";
   }, []);
-  
-  return (
-    <main className="">
 
+  const navigate = useNavigate();
+
+  const voltarParaHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <main className="bg-white flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 px-4 text-center">
+      <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
+      <p className="text-xl mb-6 max-w-md">
+        Essa Página não existe!
+      </p>
+      <p className="text-xl mb-6 max-w-md">
+        Volte para a página inicial:
+      </p>
+      <button
+        onClick={voltarParaHome}
+        className="cursor-pointer px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300"
+      >
+        Voltar para a página inicial
+      </button>
     </main>
-  )
+  );
 }
